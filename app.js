e4b065430a05b33ccb57be978c58bb8c10291414
pbatsaihan <batsaihan.p@gmail.com>
2022-08-02 14:38:21 +0800

    Шоог шиддэг болгов

--- app.js
+++ app.js
@@ -8,20 +8,18 @@ var scores = [0, 0];
 var roundScore = 0;
 
 // Шооны буусан оноог хадгалагч 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүй үүсгэж өгнө.
-var dice = Math.floor(Math.random() * 6) + 1;
-
-// DOM element
-// window.document.querySelector("#score-0").textContent = dice;
-
-// document.querySelector("#score-1").innerHTML = "<em>Yes</em>";
-
-document.querySelector(".dice").style.display = "none";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 
 // Тогломоо эхлэхэд бэлтгэе
-window.document.querySelector("#score-0").textContent = 0;
-window.document.querySelector("#score-1").textContent = 0;
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
-window.document.querySelector("#current-0").textContent = 0;
-window.document.querySelector("#current-1").textContent = 0;
-
-console.log("Шоо:" + dice);
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
